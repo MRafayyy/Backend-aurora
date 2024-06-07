bb0e95af2d026b588dcaae6763f87a60b91a88a6
Muhammad Rafay Aziz <mohammadrafayaziz12345@gmail.com>
2024-06-08 02:43:26 +0500

    update

--- index.js
+++ index.js
@@ -566,7 +566,8 @@ await Promise.all([...updateRegisterPromises, ...updateContactsRegisterPromises]
   );
 };
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
   console.log("server running on port 3000");
 });
 
