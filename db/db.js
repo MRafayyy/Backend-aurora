80538b24b2bbf03fe62588ecf5cb2afbf28d1394
mrafayyy <bscs2012354@szabist.pk>
2024-07-31 00:31:48 +0500

    updated ignore

--- db/db.js
+++ db/db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
 require('dotenv').config()
-//first oyKxoUadefks1uIO
-// Jxz556eRnvEqLdGs
 let mongoServer = process.env.MONGO_DB_SERVER_KEY
 let url = mongoServer
-// ,{useNewUrlParser: true, useUnifiedTopology: true}
 mongoose.connect(url).then(()=>{
     console.log("mongoose connected")
 }).catch((error)=>{
